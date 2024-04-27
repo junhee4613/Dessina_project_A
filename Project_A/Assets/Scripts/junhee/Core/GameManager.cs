28d28b430fc9592caff55a0701d5a7537d8eada8
junhee4613 <junhee000123@naver.com>
2024-04-27 23:41:16 +0900

    중간 푸시

--- Project_A/Assets/Scripts/junhee/Core/GameManager.cs
+++ Project_A/Assets/Scripts/junhee/Core/GameManager.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public class GameManager
 {
     public bool game_over = false;
-    public float player_hp = 100;
+    public float player_hp = 3;
 }
--- Project_A/Assets/Scripts/junhee/Player_controller_junhee.cs
+++ Project_A/Assets/Scripts/junhee/Player_controller_junhee.cs
@@ -6,11 +6,15 @@ public class Player_controller_junhee : MonoBehaviour
 {
     public Rigidbody rb;
     public float boost_force;
+    public float max_speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(max_speed == 0)
+        {
+            Debug.LogError("최대 스피드 제한을 설정 안했음");
+        }
     }
 
     // Update is called once per frame
@@ -21,17 +25,18 @@ public class Player_controller_junhee : MonoBehaviour
             Managers.GameManager.game_over = true;
             Die();
         }
-        Boost(boost_force);
+        Boost(ref boost_force);
     }
-    public void Boost(float boost)
+    public void Boost(ref float boost)
     {
-        if (Input.GetAxis("Horizontal") != 0)
+        if (Input.GetAxis("Horizontal") != 0|| Input.GetAxis("Vertical") != 0)
         {
-            rb.velocity = new Vector3(Input.GetAxis("Horizontal") * boost, rb.velocity.y, 0);
+            rb.velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0).normalized * boost;
+            boost = Mathf.Clamp(boost + Time.deltaTime, 0, max_speed);
         }
-        if (Input.GetAxis("Vertical") != 0)
+        else
         {
-            rb.velocity = new Vector3(rb.velocity.x, Input.GetAxis("Vertical") * boost, 0);
+            boost = 0;
         }
     }
     public void Die()
